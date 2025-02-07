855c271c5d69bb90893a93f62bc39f6b6ab8ca43
AdelGann <developer2@agents4future.com>
2025-02-07 12:44:00 -0400

    feat: app.module configurated

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,19 @@ import { AppService } from './app.service';
 import { DbModule } from './common/db/db.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { configModule } from './common/config/config.service';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './auth/guards/Roles.guard';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
-  imports: [DbModule, UsersModule, AuthModule],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
+  imports: [DbModule, UsersModule, AuthModule, configModule],
 })
 export class AppModule {}
